a4fc075f12fec061823f6c802152437fa59bccfb
Marcelo Silva <mjmarce@gmail.com>
2022-04-14 16:21:40 -0300

    Criando tela de listagem de eventos

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+#redirecionando agenda para pagina principal
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agenda/',views.lista_eventos),
+    path('', RedirectView.as_view(url='/agenda/'))
 ]
--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class Evento (models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null = True)
     data_evento = models.DateField(verbose_name= 'Data do Evento')
-    data_criacao = models.DateTimeField(auto_now=True)
+    data_criacao = models.DateTimeField(auto_now = True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     #NOME DA TABELA A SER CRIADA
@@ -14,3 +14,6 @@ class Evento (models.Model):
     # tratando o nome do evento exibindo o nome
     def __str__(self):
         return self.titulo
+    #função que formata data e hota
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
