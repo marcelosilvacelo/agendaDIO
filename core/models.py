d608d5d65d73e4958d821ca904dd16f2f6183879
Marcelo Silva <mjmarce@gmail.com>
2022-04-19 16:21:35 -0300

    incluido template de novo evento sem hora, e gitignore

--- core/models.py
+++ core/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User#modulo usuario django
+
 # Create your models here.
 class Evento (models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null = True)
-    data_evento = models.DateField(verbose_name= 'Data do Evento')
+    data_evento = models.DateField(verbose_name='Data do evento')
     data_criacao = models.DateTimeField(auto_now = True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -16,4 +17,4 @@ class Evento (models.Model):
         return self.titulo
     #função que formata data e hota
     def get_data_evento(self):
-        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+        return self.data_evento.strftime("%d/%m/%Y, %H:%M:%S")
